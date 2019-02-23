dd48317033e787793893eb8d87b5c74e76f4b402
pstilian <pstilian111@gmail.com>
2019-02-23 17:51:12 -0500

    first updates to WallFollowing. unsure about left and right turn using basic intialization similar to walldistance.py

--- Lab2/wallFollowing.py
+++ Lab2/wallFollowing.py
@@ -10,10 +10,56 @@ import math
 import Adafruit_PCA9685
 
 # variables
-leftflag = False
-rightflag = False
-dAxis = 3.95
-
+lCount = 0
+rCount = 0
+#TotalCount = (0, 0)
+lSpeed = 0
+rSpeed = 0
+lRevolutions = 0
+rRevolutions = 0
+startTime = time.time()
+currentTime = 0
+dAxis = float(3.95)
+LWSpeed = {
+            0.00: 1.50, 0.01: 1.505, 0.02: 1.505, 0.03: 1.51, 0.04: 1.51,
+            0.05: 1.51, 0.06: 1.51, 0.07: 1.51, 0.08: 1.515, 0.09: 1.515,
+            0.10: 1.515, 0.11: 1.515, 0.12: 1.515, 0.13: 1.52, 0.14: 1.52,
+            0.15: 1.52, 0.16: 1.52, 0.17: 1.52, 0.18: 1.525, 0.19: 1.525,
+            0.20: 1.525, 0.21: 1.525, 0.22: 1.525, 0.23: 1.525, 0.24: 1.53,
+            0.25: 1.53, 0.26: 1.53, 0.27: 1.53, 0.28: 1.53, 0.29: 1.535,
+            0.30: 1.535, 0.31: 1.535, 0.32: 1.535, 0.33: 1.54, 0.34: 1.54,
+            0.35: 1.54, 0.36: 1.54, 0.37: 1.54, 0.38: 1.545, 0.39: 1.545,
+            0.40: 1.545, 0.41: 1.545, 0.42: 1.545, 0.43: 1.55, 0.44: 1.55,
+            0.45: 1.55, 0.46: 1.55, 0.47: 1.56, 0.48: 1.555, 0.49: 1.555,
+            0.50: 1.555, 0.51: 1.555, 0.52: 1.555, 0.53: 1.56, 0.54: 1.56,
+            0.55: 1.56, 0.56: 1.56, 0.57: 1.56, 0.58: 1.565, 0.59: 1.565,
+            0.60: 1.565, 0.61: 1.565, 0.62: 1.565, 0.63: 1.57, 0.64: 1.57,
+            0.65: 1.57, 0.66: 1.57, 0.67: 1.57, 0.68: 1.575, 0.69: 1.575,
+            0.70: 1.58, 0.71: 1.58, 0.72: 1.585, 0.73: 1.585, 0.74: 1.59,
+            0.75: 1.59, 0.76: 1.59, 0.77: 1.60, 0.78: 1.60, 0.79: 1.61,
+            0.80: 1.61, 0.81: 1.61, 0.82: 1.61, 0.83: 1.62, 0.84: 1.63,
+            0.85: 1.64, 0.86: 1.65, 0.87: 1.70
+            }
+RWSpeed = {
+            0.00: 1.50, 0.01: 1.50, 0.02: 1.50, 0.03: 1.505, 0.04: 1.505,
+            0.05: 1.505, 0.06: 1.505, 0.07: 1.505, 0.08: 1.51, 0.09: 1.51,
+            0.10: 1.51, 0.11: 1.51, 0.12: 1.51, 0.13: 1.515, 0.14: 1.515,
+            0.15: 1.515, 0.16: 1.515, 0.17: 1.515, 0.18: 1.52, 0.19: 1.52,
+            0.20: 1.52, 0.21: 1.52, 0.22: 1.52, 0.23: 1.525, 0.24: 1.525,
+            0.25: 1.525, 0.26: 1.525, 0.27: 1.52, 0.28: 1.53, 0.29: 1.53,
+            0.30: 1.53, 0.31: 1.53, 0.32: 1.53, 0.33: 1.535, 0.34: 1.535,
+            0.35: 1.535, 0.36: 1.535, 0.37: 1.54, 0.38: 1.54, 0.39: 1.54,
+            0.40: 1.54, 0.41: 1.54, 0.42: 1.54, 0.43: 1.5425, 0.44: 1.5425,
+            0.45: 1.5425, 0.46: 1.545, 0.47: 1.545, 0.48: 1.5475, 0.49: 1.5475,
+            0.50: 1.5475, 0.51: 1.5475, 0.52: 1.55, 0.53: 1.55, 0.54: 1.55,
+            0.55: 1.55, 0.56: 1.555, 0.57: 1.555, 0.58: 1.555, 0.59: 1.56,
+            0.60: 1.56, 0.61: 1.56, 0.62: 1.56, 0.63: 1.56, 0.64: 1.565,
+            0.65: 1.565, 0.66: 1.565, 0.67: 1.565, 0.68: 1.57, 0.69: 1.57,
+            0.70: 1.57, 0.71: 1.57, 0.72: 1.575, 0.73: 1.575, 0.74: 1.58,
+            0.75: 1.58, 0.76: 1.58, 0.77: 1.59, 0.78: 1.59, 0.79: 1.60,
+            0.80: 1.60, 0.81: 1.60, 0.82: 1.61, 0.83: 1.62, 0.84: 1.62,
+            0.85: 1.62, 0.86: 1.65, 0.87: 1.70
+            }
 
 # Pins that the encoders are connected to
 LENCODER = 17
@@ -77,129 +123,110 @@ fSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
 
 
 def ctrlC(signum, frame):
-    print("Exiting")
+      print("Exiting")
     
-    GPIO.cleanup()
-    # Write an initial value of 1.5, which keeps the servos stopped.
-    # Due to how servos work, and the design of the Adafruit library,
-    # The value must be divided by 20 and multiplied by 4096.
-    pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
-    pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+      GPIO.cleanup()
+      # Write an initial value of 1.5, which keeps the servos stopped.
+      # Due to how servos work, and the design of the Adafruit library,
+      # The value must be divided by 20 and multiplied by 4096.
+      pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
+      pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+      lSensor.stop_ranging()
+      fSensor.stop_ranging()
+      rSensor.stop_ranging()
 
-    exit()
+      exit()
 
-
-def setSpeedsRPS(rpsLeft, rpsRight):
-    # needs to convert from RPS to PWM values
-    global leftflag, rightflag
-    # Calculating pwm values from the respective dictionaries
-    left = round(float(rpsLeft), 2)#.55
-    right = round(float(rpsRight), 2)#.55
-    leftflag = False
-    rightflag = False
-
-    
-    # Loop compares rpsLeft values with rps values from calibration document
-    while leftflag != True:
-
-        l = open("LeftSpeedCalibration.txt", "r")
-        for line in l:
-            currentLine = line.split(" ")
-            rpsValue = round(float(currentLine[0]), 2)
-            pwmValue = round(float(currentLine[1]), 2)
-
-            if left == rpsValue:
-                lPwmValue = pwmValue
-                print("LPWMVALUE: ", lPwmValue)
-                leftflag = True
-                break
-            elif left > 0.78:
-                lPwmValue = 0
-                leftflag = False
-                break
-        l.close()
-        left = round(left + 0.01, 2)
-
-    # Loop compares rpsRight values with rps values from calibration document
-    while rightflag != True:
-
-        r = open("RightSpeedCalibration.txt", "r")
-        for line in r:
-            currentLine = line.split(" ")
-            rpsValue = round(float(currentLine[0]), 2)
-            pwmValue = round(float(currentLine[1]), 2)
-
-            if right == rpsValue:
-                rPwmValue = pwmValue
-                print("RPWMVALUE: ", rPwmValue)
-                rightflag = True
-                break
-            elif right > 0.80:
-                rPwmValue = 0
-                rightflag = False
-                break
-        r.close()
-        right = round(right + 0.01, 2)
-        
-    # If both right and left values are found then statement sets speeds to desired imputs
-    if rightflag == True and leftflag == True:
-        # Setting appropiate speeds to the servos
-        pwm.set_pwm(LSERVO, 0, math.floor(setDifference(lPwmValue) / 20 * 4096))
-        pwm.set_pwm(RSERVO, 0, math.floor(rPwmValue / 20 * 4096))
+def setDifference(speed):
+    diff = speed - 1.5
+    return 1.5 - diff
         
 # Sets speed of motors in Inches per econd
 def setSpeedsIPS(ipsLeft, ipsRight):
-    # Function sets speed of robot to move over a linear speed with a set angular velocity
-    # v = inches per second         w = angular velocity
-    # positive w values spin counterclockwise       negative w values spin clockwise
-    rpsLeft = round(float(ipsLeft / 8.20), 2)
-    rpsRight = round(float(ipsRight / 8.20), 2)
-    
-    #Calculates the PWM values by using RPS
-    setSpeedsRPS(rpsLeft, rpsRight)
+    # Converting inches per second into revolutions per second
+    rpsLeft = float(math.ceil((ipsLeft / 8.20) * 100) / 100)
+    rpsRight = float(math.ceil((ipsRight / 8.20) * 100) / 100)
 
-# Defines speeds in a clockwise rotation
+    if rpsLeft < 0:
+        rpsLeft = 0 - rpsLeft
+    if rpsRight < 0:
+        rpsRight = 0 - rpsRight
+
+    # Calculating pwm values from the respective dictionaries
+    lPwmValue = float(LWSpeed[rpsLeft])
+    rPwmValue = float(RWSpeed[rpsRight])
+
+    if ipsLeft < 0 or ipsRight < 0:
+        # Setting appropiate speeds to the servos when going forwards
+        pwm.set_pwm(LSERVO, 0, math.floor(lPwmValue / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(setDifference(rPwmValue) / 20 * 4096))
+    elif ipsLeft >= 0 or ipsRight >= 0:
+        # Setting apporpiate speeds to the servos when going backwards
+        pwm.set_pwm(LSERVO, 0, math.floor(setDifference(lPwmValue) / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(rPwmValue / 20 * 4096))
+
+
+###############################
+def saturationFunction(ips):
+    controlSignal = ips
+    if controlSignal > 0.5:
+        controlSignal = 0.5
+    elif controlSignal < -0.5:
+        controlSignal = -0.5
+    return controlSignal
+
+def leftTurn():#####################
+
+def rightTurn():###################
+
 def setSpeedsvw(v, w):
-	velocityLeft = ( v + ( w * dAxis))
-	velocityRight = ( v - ( w * dAxis))
+      velocityLeft1 = float(( v + ( w * dAxis)))
+      velocityRight1 = float(( v - ( w * dAxis)))
 
-	setSpeedsIPS(velocityLeft, velocityRight)
+      setSpeedsIPS(velocityLeft1, velocityRight1)
 
-
-#--------------------------------------MAINLINE CODE----------------------------------------------------
 desiredDistance = 5.0
-#kpValue = 0.9
+kpValue = 0.9
+
+# Attach the Ctrl+C signal interrupt and initialize encoders
+signal.signal(signal.SIGINT, ctrlC)
 
 # Initialized servos to zero movement
 pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
-pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
-time.sleep(2)
+pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+time.sleep(5)
 
-# Command to begin running the program
+#--------------------------------------MAINLINE CODE----------------------------------------------------
 startFlag = False
-selectCommand = input("Please enter \'s\' to begin robot movement: ")
-if selectCommand == "s":
-	startFlag = True
-else:
-	print("Invalid Command Exiting the program")
-	exit()
+selectCommand = ' '
 
-#linearSpeed = 5
+while selectCommand != 's':
+      selectCommand = input("Please enter \'s\' to begin robot movement: ")
+
+startFlag =True
+
+# Set Linear Speed to 5 inches per second
+linearSpeed = 5
 
 while True:
-	fDistance = fSensor.get_distance()
-	# rDistance = rSensor.get_distance()
-	inchesDistanceF = fDistance * 0.0393700787 		# why this number?
-	# inchesDistanceR = rDistance * 0.0393700787
+      # Reads Distance From Sensors
+      fDistance = fSensor.get_distance()
+      rDistance = rSensor.get_distance()
+      #lDistance = lSensor.get_distance()
 
-	print("FRONT DISTANCE: ", inchesDistanceF)
+      # Converts readings from centimeters to inches
+      fInchDistance = fDistance * 0.3937
+      rInchDistance = rDistance * 0.3937
+      # 0.394 is the conversion rate from cm to inches Determining error amount
 
-	fError = 5.0 - inchesDistanceF
-	# rError = 5.0 - inchesDistanceR
+      # fError is the calculated respective error value aka the e(t) value
+      fError = desiredDistance - fInchDistance
+      rError = desiredDistance - rInchDistance
 
-	setSpeedsvw(linearSpeed, linearSpeed)			# needs to be modified goes straight as test case
+      # Control Signal aka u(t)  = Kp * e(t)
+      fControlSignal = kpValue * fError
+      rControlSignal = kpValue * rError
 
-	# if inchesDistanceF < 5.0:						corrects if robot is too far from wall
-	#	turnLeft()									need to create this function
-
-	if 
+      # Calculating new control signal value by running control signal through saturation function
+      fNewSignal = saturationFunction(fControlSignal)
+      rNewSignal = saturationFunction(rControlSignal)
