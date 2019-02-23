56720105d2058a08b061512739bca312c4516e4c
pstilian <pstilian111@gmail.com>
2019-02-23 15:26:18 -0500

    modified wall forward.py

--- Lab2/wallDistance.py
+++ Lab2/wallDistance.py
@@ -125,52 +125,11 @@ def setSpeedsRPS(rpsLeft, rpsRight):
     # Calculating pwm values from the respective dictionaries
     left = round(float(rpsLeft), 2)#.55
     right = round(float(rpsRight), 2)#.55
-    leftflag = False
-    rightflag = False
+
 
     
     # Loop compares rpsLeft values with rps values from calibration document
-    while leftflag != True:
 
-        l = open("LeftSpeedCalibration.txt", "r")
-        for line in l:
-            currentLine = line.split(" ")
-            rpsValue = round(float(currentLine[0]), 2)
-            pwmValue = round(float(currentLine[1]), 2)
-
-            if left == rpsValue:
-                lPwmValue = pwmValue
-                print("LPWMVALUE: ", lPwmValue)
-                leftflag = True
-                break
-            elif left > 0.78:
-                lPwmValue = 0
-                leftflag = False
-                break
-        l.close()
-        left = round(left + 0.01, 2)
-
-    # Loop compares rpsRight values with rps values from calibration document
-    while rightflag != True:
-
-        r = open("RightSpeedCalibration.txt", "r")
-        for line in r:
-            currentLine = line.split(" ")
-            rpsValue = round(float(currentLine[0]), 2)
-            pwmValue = round(float(currentLine[1]), 2)
-
-            if right == rpsValue:
-                rPwmValue = pwmValue
-                print("RPWMVALUE: ", rPwmValue)
-                rightflag = True
-                break
-            elif right > 0.80:
-                rPwmValue = 0
-                rightflag = False
-                break
-        r.close()
-        right = round(right + 0.01, 2)
-        
     # If both right and left values are found then statement sets speeds to desired imputs
     if rightflag == True and leftflag == True:
         # Setting appropiate speeds to the servos
@@ -184,24 +143,76 @@ def setSpeedsIPS(ipsLeft, ipsRight):
     # positive w values spin counterclockwise       negative w values spin clockwise
     rpsLeft = round(float(ipsLeft / 8.20), 2)
     rpsRight = round(float(ipsRight / 8.20), 2)
+    leftflag = False
+    rightflag = False
+
+    if rpsLeft < 0:
+        rpsLeft = 0 - rpsLeft
+    if rpsRight < 0:
+        rpsRight = 0 - rpsRight
+
+    #Calculating pwm values by pulling from calibration text file
+    while leftflag != True:
+
+        l = open("LeftSpeedCalibration.txt", "r")
+        for line in l:
+            currentLine = line.split(" ")
+            rpsValue = round(float(currentLine[0]), 2)
+            pwmValue = round(float(currentLine[1]), 2)
+
+            if rpsLeft == rpsValue:
+                lPwmValue = pwmValue
+                print("LPWMVALUE: ", lPwmValue)
+                leftflag = True
+                break
+            elif rpsLeft > 0.78:
+                lPwmValue = 0
+                leftflag = False
+                break
+        l.close()
+        rpsLeft = round(left + 0.01, 2)
+
+    # Loop compares rpsRight values with rps values from calibration document
+    while rightflag != True:
+
+        r = open("RightSpeedCalibration.txt", "r")
+        for line in r:
+            currentLine = line.split(" ")
+            rpsValue = round(float(currentLine[0]), 2)
+            pwmValue = round(float(currentLine[1]), 2)
+
+            if rpsRight == rpsValue:
+                rPwmValue = pwmValue
+                print("RPWMVALUE: ", rPwmValue)
+                rightflag = True
+                break
+            elif rpsRight > 0.80:
+                rPwmValue = 0
+                rightflag = False
+                break
+        r.close()
+        rpsRight = round(right + 0.01, 2)
+
+
+    if rpsLeft < 0 or rpsRight < 0:
+        pwm.set_pwm(LSERVO, 0,math.floor(lPwmValue / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0,math.floor(setDifference(rPwmValue)/ 20 * 4096))
+    elif ipsLeft >= 0 or ipsRight >= 0:
+        pwm.set_pwm(LSERVO, 0,math.floor(setDiifference(lPwmValue) / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0,math.floor(rPwmValue / 20 * 4096))
     
     #Calculates the PWM values by using RPS
-    setSpeedsRPS(rpsLeft, rpsRight)
+    #setSpeedsRPS(rpsLeft, rpsRight)
 
-# Defines speeds in a clockwise rotation
-def setSpeedsvw(v, w):
-	velocityLeft = ( v + ( w * dAxis))
-	velocityRight = ( v - ( w * dAxis))
 
-	setSpeedsIPS(velocityLeft, velocityRight)
 
 ###############################
 def saturationFunction(ips):
     signal = ips
-    if signal > 7.1:
-        signal = 7.1
-    elif signal < -7.1:
-        signal = -7.1
+    if signal > 7.0:
+        signal = 7.0
+    elif signal < -7.0:
+        signal = -7.0
     return signal
 
 
@@ -229,8 +240,12 @@ while True:
     # Control Signal aka u(t)  = Kp * e(t)
     controlSignal = kpValue * fError
 
+    print("controlSignal = ", controlSignal)
+
     # Calculating new control signal value by running control signal through saturation function
     newSignal = saturationFunction(controlSignal)
+
+    print("newSignal = ", newSignal)
 
     setSpeedsIPS(newSignal, newSignal)
 
