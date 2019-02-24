9ab3b6784d513307684f9b6d0f7ab90b91522c73
pstilian <pstilian111@gmail.com>
2019-02-23 21:18:29 -0500

    issues might of been that i was converting sensor readings from centimeters to inches instead of from milimeters

--- Lab2/wallDistance.py
+++ Lab2/wallDistance.py
@@ -138,6 +138,7 @@ def setSpeedsIPS(ipsLeft, ipsRight):
     rpsLeft = float(math.ceil((ipsLeft / 8.20) * 100) / 100)
     rpsRight = float(math.ceil((ipsRight / 8.20) * 100) / 100)
 
+    # makes sure RPS is always a positive number
     if rpsLeft < 0:
         rpsLeft = 0 - rpsLeft
     if rpsRight < 0:
@@ -147,11 +148,11 @@ def setSpeedsIPS(ipsLeft, ipsRight):
     lPwmValue = float(LWSpeed[rpsLeft])
     rPwmValue = float(RWSpeed[rpsRight])
 
-    if ipsLeft < 0 or ipsRight < 0:
+    if ipsLeft < 0 and ipsRight < 0:
         # Setting appropiate speeds to the servos when going forwards
         pwm.set_pwm(LSERVO, 0, math.floor(lPwmValue / 20 * 4096))
         pwm.set_pwm(RSERVO, 0, math.floor(setDifference(rPwmValue) / 20 * 4096))
-    elif ipsLeft >= 0 or ipsRight >= 0:
+    elif ipsLeft >= 0 and ipsRight >= 0:
         # Setting apporpiate speeds to the servos when going backwards
         pwm.set_pwm(LSERVO, 0, math.floor(setDifference(lPwmValue) / 20 * 4096))
         pwm.set_pwm(RSERVO, 0, math.floor(rPwmValue / 20 * 4096))
@@ -187,8 +188,8 @@ while True:
 
     print("FRONT DISTANCE : ", fDistance)
 
-    # Converts readings from centimeters to inches
-    inchDistance = fDistance * 0.3937
+    # Converts readings from milimeters to inches
+    inchDistance = fDistance * 0.03937
     # 0.394 is the conversion rate from cm to inches Determining error amount
 
     # fError is the calculated respective error value aka the e(t) value
