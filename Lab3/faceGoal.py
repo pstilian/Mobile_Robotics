00927017a935c0ffd3fadbb2e6e1ca44b97fd2ad
pstilian <donaldc354@gmail.com>
2019-03-22 16:14:27 -0400

    working on fgoal

--- Lab3/faceGoal.py
+++ Lab3/faceGoal.py
@@ -302,6 +302,7 @@ while True:
     if keypoints:
         pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
         pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+        
     if not keypoints:
         pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
         pwm.set_pwm(RSERVO, 0, math.floor(1.51 / 20 * 4096))
