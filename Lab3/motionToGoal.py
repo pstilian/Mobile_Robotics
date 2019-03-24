91c005967d0808ee354885082e44ba23a282b41c
pstilian <donaldc354@gmail.com>
2019-03-23 23:32:06 -0400

    fucking damnit

--- Lab3/motionToGoal.py
+++ Lab3/motionToGoal.py
@@ -200,8 +200,8 @@ def onMaxVTrackbar(val):
 
 def targetFinder():
     if x_pos >= 250 and x_pos <= 310:
-		pwm.set_pwm(LSERVO, 0, math.floor(1.50 / 20 * 4096))
-		pwm.set_pwm(RSERVO, 0, math.floor(1.50 / 20 * 4096))
+        pwm.set_pwm(LSERVO, 0, math.floor(1.50 / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(1.50 / 20 * 4096))
         trackFlag = True
 
 def motionToGoal():
