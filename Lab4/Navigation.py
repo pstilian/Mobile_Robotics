a038385df6e865e83ffd8f93ce134cac49d3802a
pstilian <pstilian111@gmail.com>
2019-04-13 14:54:28 -0400

    merby

--- Lab4/Navigation.py
+++ Lab4/Navigation.py
@@ -301,66 +301,6 @@ def whatDo(lWallOpen, fWallOpen, rWallOpen):
 		print("OH NO IM TRAPPED!! 0.0")
 		turnAround()
 
-#Check if specific flags are set to decide action.
-def whatToDo(leftWallOpen, frontWallOpen, rightWallOpen):
-    option = 0
-
-    if inchesDFront < 18:
-        frontDist()
-
-    if lWallOpen and fWallOpen and rWallOpen:
-        choices = [1, 2, 3]
-        option = random.choice(choices)
-        #stop()
-
-    elif fWallOpen and rWallOpen:
-        choices = [2, 3]
-        option = random.choice(choices)
-        #stop()
-
-    elif lWallOpen and rWallOpen:
-        choices = [1, 3]
-        option = random.choice(choices)
-        #stop()
-
-    elif lWallOpen and fWallOpen:
-        choices = [1, 2]
-        option = random.choice(choices)
-        #stop()
-
-    elif rWallOpen:
-        option = 3
-        #stop()
-
-    elif fWallOpen:
-        option = 2
-        #stop()
-
-    elif lWallOpen:
-        option = 1
-        #stop()
-
-    else:
-        option = 0
-        #stop()
-
-    #CHECK WHAT MOVE I MUST DO!
-    if option == 1:
-        #turn left
-        print("LEFT TURN")
-        leftPivot()
-    elif option == 2:
-        #keep moving forward
-        print("MOVE FORWARD")
-    elif option == 3:
-        #right turn
-        print("RIGHT TURN")
-        rightPivot()
-    else:
-        #360 turn and move forward
-        print("WALL IN ALL THREE PLACES")
-        turnAround()
-
 
 def stop():
 	pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
@@ -444,9 +384,9 @@ def moveForward():
     rDistance = rSensor.get_distance()
 
 	# Converts readings from milimeters to inches
-    finchDistance = fDistance * 0.03937
-    linchDistance = lDistance * 0.03937
-    rinchDistance = rDistance * 0.03937
+    fInchDistance = fDistance * 0.03937
+    lInchDistance = lDistance * 0.03937
+    rInchDistance = rDistance * 0.03937
 
     # fError is the calculated respective error value aka the e(t) value
     fError = 7.0 - fInchDistance
